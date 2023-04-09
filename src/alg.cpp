3f832dd83a4dcf6c5f87853be5e0dead3801bd61
Pisarevskaya-Lubov <125238314+Pisarevskaya-Lubov@users.noreply.github.com>
2023-04-09 09:26:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,58 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int countPairs1(int* arr, int len, int value)
+{
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i+1; j < len; j++) {
+       if ((arr[i] + arr[j]) == value) {
+        count++;
+       }
+    }
+  }
+  return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+int countPairs2(int* arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
+      if (arr[i] + arr[j] == value) {
+        count += 1;
+      }
+      else if (arr[i] + arr[j] > value) {
+        break;
+      }
+    }
+    continue;
+  }
+  return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+int countPairs3(int* arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    int begin = i, end = len;
+    while (end - 1 > begin) {
+      int mid = (begin + end) / 2;
+      if (arr[i] + arr[mid] == value) {
+        count++;
+        int mid2 = mid + 1;
+        while (arr[i] + arr[mid2] == value && mid2 < end) {
+          count++;
+          mid2++;
+        }
+        mid2 = mid - 1;
+        while (arr[i] + arr[mid2] == value && mid2 > begin) {
+          count++;
+          mid2--;
+        }
+        break;
+      }
+      if (arr[i] + arr[mid] > value) {
+        end = mid;
+      }
+      else {
+        begin = mid;
+      }
+    }
+  }
+  return count;
 }
