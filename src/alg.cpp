101bb2eb50db5debd72884fd21b6498f37e926b8
Pisarevskaya-Lubov <125238314+Pisarevskaya-Lubov@users.noreply.github.com>
2023-05-26 20:08:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,59 +1,75 @@
-// Copyright 2021 NNTU-CS
-int countPairs1(int* arr, int len, int value)
-{
-  int count = 0;
-  for (int i = 0; i < len; i++) {
-    for (int j = i+1; j < len; j++) {
-       if ((arr[i] + arr[j]) == value) {
-        count++;
-       }
+void Sort(int arr[], int size) {
+    int klol = 0;
+    int a = 0;
+    for (int j = 1; j < size; j++) {
+        klol = arr[j];
+        a = j - 1;
+        while (a >= 0 && arr[a] > klol) {
+            arr[a + 1] = arr[a];
+            a = a - 1;
+            arr[a + 1] = klol;
+        }
     }
-  }
-  return count;
+}
+
+int countPairs1(int* arr, int len, int value) {
+    Sort(arr, len);
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = len-1; j > i; --j) {
+            if (arr[i] + arr[j] == value) {
+                count += 1;
+            } else if (arr[i] + arr[j] < value) {
+                break;
+            }
+        }
+        continue;
+    }
+    return count;
 }
 int countPairs2(int* arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; ++i) {
-    for (int j = i + 1; j < len; ++j) {
-      if (arr[i] + arr[j] == value) {
-        count += 1;
-      }
-      else if (arr[i] + arr[j] > value) {
-        break;
-      }
+    Sort(arr, len);
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i+1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                count += 1;
+            } else if (arr[i] + arr[j] > value) {
+                break;
+            }
+        }
+        continue;
     }
-    continue;
-  }
-  return count;
+    return count;
 }
 int countPairs3(int* arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    int begin = i, end = len;
-    while (end - 1 > begin) {
-      int mid = (begin + end) / 2;
-      if (arr[i] + arr[mid] == value) {
-        count++;
-        int mid2 = mid + 1;
-        while (arr[i] + arr[mid2] == value && mid2 < end) {
-          count++;
-          mid2++;
+    Sort(arr, len);
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        int left = i, rig = len;
+
+        while (1 < rig - left) {
+            int center = (left + rig) / 2;
+            if (arr[i] + arr[center] == value) {
+                count++;
+                int j = center + 1;
+                while (arr[i] + arr[j] == value && j < rig) {
+                    count++;
+                    j++;
+                }
+                j = center - 1;
+                while (arr[i] + arr[j] == value && j > left) {
+                    count++;
+                    j--;
+                }
+                break;
+            }
+            if (arr[i] + arr[center] > value) {
+                rig = center;
+            } else {
+                left = center;
+            }
         }
-        mid2 = mid - 1;
-        while (arr[i] + arr[mid2] == value && mid2 > begin) {
-          count++;
-          mid2--;
-        }
-        break;
-      }
-      if (arr[i] + arr[mid] > value) {
-        end = mid;
-      }
-      else {
-        begin = mid;
-      }
     }
-  }
-  return count;
-  
+    return count;
 }
